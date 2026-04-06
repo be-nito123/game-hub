27c7af50f041333b49ed932d90130e8724cdae59
benito <jbbenito645@gmail.com>
2026-04-06 15:44:51 +0530

    Updated src files

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "1a75d71e45f24d9a99662743233ed2e3",
+    key: "0bed477348be4065bf1fa9e48021482c",
   },
 });
